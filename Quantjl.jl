05df07eaab22d69b1a0953dbc1d68ba90b8dc9ec
Vathy M. Kamulete <vathymut@gmail.com>
2014-11-01 02:17:13 -0400

    Added README.

--- Quantjl.jl
+++ Quantjl.jl
@@ -1,19 +1,11 @@
-# This code is in the julia port from the R package FinCal.
-# Planning to next port fucntions from the F# module Quantifa.
-
-# Author: Vathy M. Kamulete
-# Email: vathymut@gmail.com
-# Github: github.com/vathymut
-
-module FinCal
-
+module Quantjl
 export
   ear, compound_ear, ear_continuous, ear2hpr,
   r_continuous, r_norminal, r_perpetuity, r_simple,
   fv_simple, fv_annuity, fv, fv_uneven, fv_from_spot,
   pv_simple, pv_annuity, pv, pv_uneven, pv_from_spot, pv_perpetuity,
   pmt, discount_rate, n_periods, price_arbitrage, intra_accrint,
-  ear2bey, bey2ear, ytm
+  ear2bey, bey2ear, ytm, dirty_price, clean_price
 
 include("ear.jl")
 include("rates.jl")
@@ -21,6 +13,6 @@ include("fv.jl")
 include("pv.jl")
 include("pricing.jl")
 include("bonds.jl")
-
 end
+
 
---
+++ README.md
@@ -0,0 +1,22 @@
+# README
+
+Code written in [Julia](http://julialang.org/) for Computational Finance.
+
+I'm writing it in `Julia`, rather than `R`, `MATLAB` or `Python`,
+as a way to force myself to learn `Julia` and study for
+the [FRM](http://www.garp.org/frm/frm-program.aspx)
+
+## Influences
+The code is heavily influenced by:
+- FinCal (R package).
+- Quantifa (F# project).
+- Etc.
+
+## Tests
+The tests in `test.jl` were culled from different sources:
+- The R FinCal package
+- The Quantifa project
+- Several online classes
+- Etc
+
+
