e511ea12aad2fb7f49dd5a7cc164bddcc6da77a3
Vathy M. Kamulete <vathymut@gmail.com>
2015-02-26 01:05:00 -0500

    Edited error message in helper.jl

--- helper.jl
+++ helper.jl
@@ -1,21 +1,19 @@
 
 # Check that no argument is nothing
-# validate_kwargs( r, pmt, g, pmt_type )
 function validate_kwargs( a... )
-  msg_error = "Must provide all arguments; one default argument is set to nothing."
-  !any( [ isequal( var, nothing ) for var in a ] ) || error( msg_error )
+  msg_error = "Must provide all arguments; one default argument is nothing."
+  check_nothing = [ isequal( var, nothing ) for var in a ]
+  !any( check_nothing ) || error( msg_error )
 end
 
 # Check that payment type is as expected
-# validate_pmt_type( pmt_type )
 function validate_pmt_type( pmt_type )
   msg_error = "Error: pmt_type should be 0 or 1!"
   any( [ isequal( pmt_type, val ) for val in 0:1 ] ) || error( msg_error )
 end
 
 # Check that fraction of elapsed time is as expected
-# validate_frac( pmt_type )
 function validate_frac( frac )
-  msg_error = "Error: frac must be between 0 and 1."
+  msg_error = "Error: fraction must be between 0 and 1."
   zero( frac ) <= frac <= one( frac ) || error( msg_error )
 end
