ad7545a48e75ff3440363213fad1badb7c5722db
Vathy M. Kamulete <vathymut@gmail.com>
2015-03-14 23:41:30 -0400

    Used make_kwargs_only macro.

--- timevalue.jl
+++ timevalue.jl
@@ -15,49 +15,41 @@
 include( "helper.jl" )
 
 #### Estimate present value of a perpetuity ####
-function pv_perpetuity{ T <: FloatingPoint }( r::T, pmt::T; g::T = 0.0, pmt_type::Integer = 0 )
+function pv_perpetuity{ T <: FloatingPoint }( r::T, pmt::T, g::T = 0.0, pmt_type::Integer = 0 )
   validate_pmt_type( pmt_type )
   r >= g || error( "Error: g is not smaller than r!" )
   pv_amt = ( pmt/( r-g ) )*( ( 1+r )^pmt_type )*( -1.0 )
   return pv_amt
 end
 
-function pv_perpetuity( ; r = nothing, pmt = nothing, g = nothing, pmt_type = nothing )
-    validate_kwargs( r, pmt, g, pmt_type )
-    return pv_perpetuity( r, pmt, g = g, pmt_type = pmt_type )
-end
+# Make keywords-only equivalent
+@make_kwargs_only pv_perpetuity r pmt g pmt_type
 
 #### Estimate present value (pv) of a single sum ####
 function pv_simple{ T <: FloatingPoint }( r::T, n, fv )
     return ( fv/(1+r)^n )*( -1.0 )
 end
 
-function pv_simple( ; r = nothing, n = nothing, fv = nothing )
-    validate_kwargs( r, n, fv )
-    return pv_simple( r, n, fv )
-end
+# Make keywords-only equivalent
+@make_kwargs_only pv_simple r n fv
 
 #### Estimate present value (pv) of an annuity ####
-function pv_annuity{ T <: FloatingPoint }( r::T, n, pmt; pmt_type::Integer = 0 )
+function pv_annuity{ T <: FloatingPoint }( r::T, n, pmt, pmt_type::Integer = 0 )
     validate_pmt_type( pmt_type )
     pv = ( pmt/r*( 1-1/( 1+r )^n ) )*(1 + r )^pmt_type*( -1 )
     return pv
 end
 
-function pv_annuity( ; r = nothing, n = nothing, pmt = nothing, pmt_type = nothing )
-    validate_kwargs( r, n, pmt, pmt_type )
-    return pv_annuity( r, n, pmt, pmt_type = pmt_type )
-end
+# Make keywords-only equivalent
+@make_kwargs_only pv_annuity r n pmt pmt_type
 
 #### Estimate present value (pv) ####
-function pv{ T <: FloatingPoint }( r::T, n::Integer, fv, pmt; pmt_type::Integer = 0 )
-    return pv_simple( r, n, fv ) + pv_annuity( r, n, pmt; pmt_type = pmt_type )
+function pv{ T <: FloatingPoint }( r::T, n::Integer, fv, pmt, pmt_type::Integer = 0 )
+    return pv_simple( r, n, fv ) + pv_annuity( r, n, pmt, pmt_type )
 end
 
-function pv( ; r = nothing, n = nothing, fv = nothing, pmt = nothing, pmt_type = nothing )
-    validate_kwargs( r, n, fv, pmt, pmt_type )
-    return pv( r, n, fv, pmt, pmt_type = pmt_type )
-end
+# Make keywords-only equivalent
+@make_kwargs_only pv r n fv pmt pmt_type
 
 #### Computing the present value of an uneven cash flow series ####
 function pv_uneven{ T <: FloatingPoint }( r::T, cf::Vector{T} )
@@ -79,15 +71,14 @@ function pv_uneven{ T <: FloatingPoint }( r::Vector{T}, cf::Vector{T} )
   return sum
 end
 
-function pv_uneven( ; r = nothing, cf = nothing )
-    validate_kwargs( r, cf )
-    return pv_uneven( r, cf )
-end
+# Make keywords-only equivalent
+@make_kwargs_only pv_uneven r cf
 
 #### Computing the present value from spot rates ####
-function pv_from_spot{ T <: FloatingPoint }( spot_rates::Vector{T}; fv::T = 1.0 )
+function pv_from_spot{ T <: FloatingPoint }( spot_rates::Vector{T}, fv::T = 1.0 )
   n = length( spot_rates )
   pv_spot = fv
+  # Better way to write this
   for i=n:-1:1
     # amount to rediscount each period = abs( pv_spot )
     pv_spot = pv_simple( spot_rates[ i ], 1, abs( pv_spot ) )
@@ -95,42 +86,34 @@ function pv_from_spot{ T <: FloatingPoint }( spot_rates::Vector{T}; fv::T = 1.0 
   return pv_spot
 end
 
-function pv_from_spot( ; spot_rates = nothing, fv = nothing )
-    validate_kwargs( spot_rates, fv )
-    return pv_from_spot( spot_rates, fv = fv )
-end
+# Make keywords-only equivalent
+@make_kwargs_only pv_from_spot spot_rates fv
 
 #### Estimate future value (fv) of a single sum ####
 function fv_simple{ T <: FloatingPoint }( r::T, n, pv::T )
   return ( pv*(1+r)^n )*( -1.0 )
 end
 
-function fv_simple( ; r = nothing, n = nothing, pv = nothing )
-    validate_kwargs( r, n, pv )
-    fv_simple( r, n, pv )
-end
+# Make keywords-only equivalent
+@make_kwargs_only fv_simple r n pv
 
 #### Estimate future value of an annuity ####
-function fv_annuity{ T <: FloatingPoint }( r::T, n, pmt; pmt_type::Integer = 0 )
+function fv_annuity{ T <: FloatingPoint }( r::T, n, pmt, pmt_type::Integer = 0 )
   validate_pmt_type( pmt_type )
   fv = ( pmt/r*( ( 1+r )^n - 1 ) )*( 1+r )^pmt_type*( -1.0 )
   return fv
 end
 
-function fv_annuity( ; r = nothing, n = nothing, pmt = nothing, pmt_type = nothing )
-    validate_kwargs( r, n, pmt, pmt_type )
-    fv_annuity( r, n, pmt, pmt_type = pmt_type )
-end
+# Make keywords-only equivalent
+@make_kwargs_only fv_annuity r n pmt pmt_type
 
 #### Estimate future value (fv) ####
-function fv{ T <: FloatingPoint }( r::T, n, pv, pmt; pmt_type::Integer = 0 )
+function fv{ T <: FloatingPoint }( r::T, n, pv, pmt, pmt_type::Integer = 0 )
   return fv_simple( r, n, pv ) + fv_annuity( r, n, pmt; pmt_type = pmt_type )
 end
 
-function fv( ; r = nothing, n = nothing, pv = nothing, pmt = nothing, pmt_type = nothing )
-    validate_kwargs( r, n, pv, pmt, pmt_type )
-    return fv( r, n, pv, pmt, pmt_type = pmt_type )
-end
+# Make keywords-only equivalent
+@make_kwargs_only fv r n pmt pmt_type
 
 #### Computing the future value of an uneven cash flow series ####
 function fv_uneven{ T <: FloatingPoint }( r::T, cf::Vector{T} )
@@ -154,15 +137,14 @@ function fv_uneven{ T <: FloatingPoint }( r::Vector{T}, cf::Vector{T} )
   return sum
 end
 
-function fv_uneven( ; r = nothing, cf = nothing )
-    validate_kwargs( r, cf )
-    return fv_uneven( r, cf )
-end
+# Make keywords-only equivalent
+@make_kwargs_only fv_uneven r cf
 
 #### Computing future value from spot rates ####
-function fv_from_spot{ T <: FloatingPoint }( spot_rates::Vector{T}; pv = -1.0 )
+function fv_from_spot{ T <: FloatingPoint }( spot_rates::Vector{T}, pv = -1.0 )
   n = length( spot_rates )
   fv_spot = pv
+  # Better way to write this
   for i=1:n
     # amout to reinvest each period = -abs( fv_spot )
     fv_spot = fv_simple( spot_rates[ i ], 1, -abs( fv_spot ) )
@@ -170,10 +152,8 @@ function fv_from_spot{ T <: FloatingPoint }( spot_rates::Vector{T}; pv = -1.0 )
   return fv_spot
 end
 
-function fv_from_spot( ; spot_rates = nothing, pv = nothing )
-    validate_kwargs( spot_rates, pv )
-    return fv_from_spot( spot_rates, pv = pv )
-end
+# Make keywords-only equivalent
+@make_kwargs_only fv_from_spot spot_rates pv
 
 #### Calculate accrued interest during period (intra) ####
 function accrint_intra_period{ T <: FloatingPoint }( r::T; frac::T = 1.0, par::T = 1.0 )
@@ -181,48 +161,40 @@ function accrint_intra_period{ T <: FloatingPoint }( r::T; frac::T = 1.0, par::T
   return par*( 1 + r )^frac - par
 end
 
-function accrint_intra_period( ; r = nothing, frac = nothing, par = nothing )
-    validate_kwargs( r, frac, par )
-    return accrint_intra_period( r; frac = frac, par = par )
-end
+# Make keywords-only equivalent
+@make_kwargs_only accrint_intra_period r, frac, par
 
 #### Estimate period payment ####
-function pmt{ T <: FloatingPoint }( r::T, n, pv, fv; pmt_type::Integer = 0 )
+function pmt{ T <: FloatingPoint }( r::T, n, pv, fv, pmt_type::Integer = 0 )
   any( [ isequal(pmt_type, val) for val in 0:1 ] ) || error( "Error: pmt_type should be 0 or 1!" )
   return ( pv+fv/( 1+r )^n )*r/( 1-1/( 1+r )^n )*(-1)*( 1+r )^( -1*pmt_type )
 end
 
-function pmt( ; r = nothing, n = nothing, pv = nothing, fv = nothing, pmt_type = nothing )
-    validate_kwargs( r, n, pv, fv, pmt_type )
-    return pmt( r, n, pv, fv, pmt_type = pmt_type )
-end
+# Make keywords-only equivalent
+@make_kwargs_only pmt r, n, pv, fv, pmt_type
 
 #### Estimate the number of periods ####
-function nper{ T <: FloatingPoint }( r::T, pv, fv; pmt = 0, pmt_type::Int = 0 )
+function nper{ T <: FloatingPoint }( r::T, pv, fv, pmt = 0, pmt_type::Int = 0 )
   r >= zero( r ) || error( "r must be positive" )
   validate_pmt_type( pmt_type )
   n = log( -1 * (fv*r-pmt* (1+r)^pmt_type)/(pv*r+pmt* (1+r)^pmt_type) )/log( 1 + r )
   return n
 end
 
-function nper( ; r = nothing, pv = nothing, fv = nothing, pmt = nothing, pmt_type = nothing )
-    validate_kwargs( r, pv, fv, pmt, pmt_type )
-    return nper( r, pv, fv, pmt = pmt, pmt_type = pmt_type )
-end
+# Make keywords-only equivalent
+@make_kwargs_only nper r, pv, fv, pmt_type
 
 #### Computing the rate of return for each period ####
 using Optim: optimize
-function discount_rate( n::Integer, price, fv, pmt; pmt_type::Integer = 0 )
+function discount_rate( n::Integer, price, fv, pmt, pmt_type::Integer = 0 )
   any( [ isequal(pmt_type, val) for val in 0:1 ] ) ||  error( "Error: pmt_type should be 0 or 1!" )
   # fmin minimizes the squared difference between the pv and the price
-  fmin( discount_rate ) = ( pv( r = discount_rate, fv = fv, n = n, pmt = pmt; pmt_type = pmt_type ) - price )^2
+  fmin( discount_rate ) = ( pv( r = discount_rate, n = n, fv = fv, pmt = pmt, pmt_type = pmt_type ) - price )^2
   return optimize( fmin, 1e-10, 1.0 ).minimum
 end
 
-function discount_rate( ; n = nothing, price = nothing, fv = nothing, pmt = nothing, pmt_type = nothing )
-    validate_kwargs( n, price, fv, pmt, pmt_type )
-    return discount_rate( n, price, fv, pmt, pmt_type = pmt_type )
-end
+# Make keywords-only equivalent
+@make_kwargs_only discount_rate r, price, fv, pmt, pmt_type
 
 #### Find the lower and upper bound of the arbitrage price ####
 function price_arbitrage{ T <: FloatingPoint }( rborrow::T, rlend::T, cf::Vector{ T } )
@@ -231,7 +203,6 @@ function price_arbitrage{ T <: FloatingPoint }( rborrow::T, rlend::T, cf::Vector
   return lb, rb
 end
 
-function price_arbitrage( ; rborrow = nothing, rlend = nothing, cf = nothing )
-    validate_kwargs( rborrow, rlend, cf )
-    return price_arbitrage( rborrow, rlend, cf )
-end
+# Make keywords-only equivalent
+@make_kwargs_only price_arbitrage rborrow, rlend, cf
+
